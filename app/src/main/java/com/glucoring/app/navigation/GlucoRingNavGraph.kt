02b0b4e41e4b3ae3cde41112106673ffd7f4a898
alireza-kha <147992687+alireza-kha@users.noreply.github.com>
2026-08-27 21:46:09 +0330

    Add files via upload

--- app/src/main/java/com/glucoring/app/navigation/GlucoRingNavGraph.kt
+++ app/src/main/java/com/glucoring/app/navigation/GlucoRingNavGraph.kt
@@ -1,5 +1,6 @@
 package com.glucoring.app.navigation
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.Favorite
